9f465a7b39937af7ed94502fd7611bd0ef8247a8
zsbenq <shuangcool314@gmail.com>
2017-03-21 21:07:48 -0500

    test3

--- src/logic_layer/Event.java
+++ src/logic_layer/Event.java
@@ -2,9 +2,9 @@ package logic_layer;
 
 public class Event {
 
+
 	public void recordRentalEvent(RentalRecord record) {
 		// TODO Auto-generated method stub
-		//test
 	}
 	
 	public void recordPaymentEvent(Order order) {
